57ad33c43352554850fae4315bc645d59caec819
Gerasimovich Igor <mgorunuch.igor@gmail.com>
2025-12-04 00:13:57 +0000

    🍺 Update rotator to 0.10.7

--- Casks/rotator.rb
+++ Casks/rotator.rb
@@ -1,9 +1,9 @@
 cask "rotator" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.1.0"
-  sha256 arm:   "REPLACE_WITH_ARM_SHA256",
-         intel: "REPLACE_WITH_INTEL_SHA256"
+  version "0.10.7"
+  sha256 arm:   "2ebebf641a1f450a48eb67ce22a00649a6613c04cc7945b777c8153b5caa289c",
+         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/mgorunuch/work-rotator-app/releases/download/v#{version}/rotator_#{version}_#{arch}.dmg"
   name "Rotator"
